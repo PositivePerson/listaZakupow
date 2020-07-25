8fb50aa7df0bf093832c4ce603ae7154cc927c47
Bartek <WonderfulLifeVision@gmail.com>
2020-07-25 23:49:15 +0200

    first commit with new webpacktemplate

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+    entry: [
+        './src/index.js',
+        './src/index.css'
+      ],
+      output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "script-loader"
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: "style-loader"
+              },
+              {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        localIdentName: "[name]__[local]___[hash:base64:5]",
+                    },	
+                  sourceMap: true
+                }
+              }
+            ]
+          }
+        ]
+      }
+    };
